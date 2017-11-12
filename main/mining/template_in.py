5328da2587ef848bf5f4898ed18e159f15e53260
BradOdenath <brad.odenath@gmail.com>
2017-11-12 08:18:58 -0500

    fuck this association

--- main/mining/template_in.py
+++ main/mining/template_in.py
@@ -2,3 +2,4 @@ class User:
     username = ""
     realname = ""
     posts = []
+    friends = []
--- main/mining/twitter_in.py
+++ main/mining/twitter_in.py
@@ -1,7 +1,8 @@
 import tweepy
-from tweepy import Stream
-from tweepy.streaming import StreamListener
+#from tweepy import Stream
+#from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
+from main.mining import template_in as TI
 
 consumer_key = 'LENjmBsK2AhttoeGCidENnWqh'
 consumer_secret = 'zkNjS3bdPek8q5NVfTSIqFRhW4ib7ra0OM824RZKpbZORQlv9W'
@@ -15,19 +16,19 @@ api = tweepy.API(auth)
 
 #Get more data based on data?  Pulled from web.
 
-class MyListener(StreamListener):
-    def on_data(self, data):
-        try:
-            with open('python.json', 'a') as f:
-                f.write(data)
-                return True
-        except BaseException as e:
-            print("Error on_data: %s" % str(e))
-        return True
-
-    def on_error(self, status):
-        print(status)
-        return True
+# class MyListener(StreamListener):
+#     def on_data(self, data):
+#         try:
+#             with open('python.json', 'a') as f:
+#                 f.write(data)
+#                 return True
+#         except BaseException as e:
+#             print("Error on_data: %s" % str(e))
+#         return True
+#
+#     def on_error(self, status):
+#         print(status)
+#         return True
 
 # sorted(set(list)) just a reference to myself, reviving python syntax.
 
@@ -63,22 +64,36 @@ class TwitterUser:
 
     def getAssociations(self): #Does not work
         stuff = []
-        friends = self.usr.friends
-        for fri in friends:
-            stuff.append(fri)
+        for friend in tweepy.Cursor(api.user_timeline(self.getUName())).items():
+            friend._json
         return stuff
+        # stuff = []
+        # friends = self.usr.friends
+        # for fri in friends:
+        #     stuff.append(fri)
+        # return stuff
 
     def getJSON(self):
-        return
+        return {
+            "platform": "1",
+            "username": str(self.getUName()),
+            "realname": str(self.getRName()),
+            "text": self.getTweets(),
+            "hyperlink": "https://wwww.twitter.com/"+self.getUName(),
+            "associations": self.getAssociations()
+        }
 
-def noSillyTrump(stuff):  # Upon your request, ignore the bad look.
+def noSillyTrump(stuff):  # Upon your request, ignore the bad look for now
     return stuff.replace("...", "").replace("..", ".").replace("…", "")
 
-def getTheJ(*stuff):
-    sub_stuff = []
-    for thing in stuff:
-        sub_stuff.append(thing.getJSON())
-    return sub_stuff
+# def getTheJ(*stuff):
+#     sub_stuff = []
+#     try:
+#         for thing in stuff:
+#             sub_stuff.append(thing._json.getJSON())
+#     except TypeError as err:
+#         print("Wrong shit")
+#     return sub_stuff
 
 prezis = [
     TwitterUser("realDonaldTrump"),
@@ -87,9 +102,15 @@ prezis = [
     TwitterUser("erictg97")
     ]
 
-def getTwitterStream(searchterm): #Need to test
-    twitter_stream = Stream(auth, MyListener())
-    twitter_stream.filter(track=[searchterm])
-    return twitter_stream
+dumpableShit = []
 
-findStuff("#hackital")
+for fuckhead in prezis:
+    dumpableShit.append(fuckhead.getJSON())
+
+print(dumpableShit[0])
+
+# def getTwitterStream(searchterm): #Need to test
+#     twitter_stream = Stream(auth, MyListener())
+#     twitter_stream.filter(track=[searchterm])
+#     return twitter_stream
+#
